a8ba60393b43fcaae67d394eef5b35853d94412e
yangra <brambazluk@gmail.com>
2017-03-25 13:30:42 +0200

    Amendment to last commit

---
+++ Javazoo-Forum/src/main/java/javazoo/forum/Controller/HomeController.java
@@ -0,0 +1,16 @@
+package javazoo.forum.Controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index(Model model){
+        model.addAttribute("view", "home/index");
+        return "base-layout";
+    }
+}
---
+++ Javazoo-Forum/src/main/java/javazoo/forum/bindingModel/UserBindingModel.java
@@ -0,0 +1,62 @@
+package javazoo.forum.bindingModel;
+
+
+import javax.validation.constraints.NotNull;
+
+public class UserBindingModel {
+
+    @NotNull
+    private String username;
+
+    @NotNull
+    private String email;
+
+    @NotNull
+    private String fullName;
+
+    @NotNull
+    private String password;
+
+    @NotNull
+    private String confirmPassword;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+}
