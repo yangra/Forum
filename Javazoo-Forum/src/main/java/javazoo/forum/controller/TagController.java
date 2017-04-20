516d0d352ec7a71823a3292bbb4e71e1f6a3d385
danielanikolova <daniela.a.nikolova@gmail.com>
2017-04-20 16:31:11 +0300

    Questions with tag view with Categories section

--- Javazoo-Forum/src/main/java/javazoo/forum/controller/TagController.java
+++ Javazoo-Forum/src/main/java/javazoo/forum/controller/TagController.java
@@ -1,6 +1,10 @@
-package javazoo.forum.controller;
+package javazoo.forum.Controller;
 
+import javazoo.forum.entity.Category;
+import javazoo.forum.entity.Question;
 import javazoo.forum.entity.Tag;
+import javazoo.forum.repository.CategoryRepository;
+import javazoo.forum.repository.QuestionRepository;
 import javazoo.forum.repository.TagRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,11 +12,18 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.List;
+
 @Controller
 public class TagController {
 
     @Autowired
     private TagRepository tagRepository;
+    @Autowired
+    private QuestionRepository questionRepository;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
 
     @GetMapping("/tag/{name}")
     public String questionsWithTag(Model model, @PathVariable String name){
@@ -23,8 +34,14 @@ public class TagController {
             return "redirect:/";
         }
 
+        List<Category> categories = this.categoryRepository.findAllByOrderByOrderNoAsc();
+       List<Question> questions = this.questionRepository.findAllByOrderByCreationDateDesc();
+
         model.addAttribute("view", "tag/questions");
         model.addAttribute("tag", tag);
+        model.addAttribute("categories",categories );
+        model.addAttribute("questions", questions);
+
 
         return "base-layout";
     }
